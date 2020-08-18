790fe7d7c0f6c5f8a0f805c5d33be413cd20ab08
Joseph397 <gramkracker88@gmail.com>
2020-08-18 16:14:11 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Logical Operators
+// (|| - OR), (&& - AND), !
+
+// const name = 'peter';
+// const age = 24;
+
+// if(name === 'bob' || age === 24) { // if name is equal to bob OR age is equal to 24
+//     console.log('hello there user');
+// } else {
+//     console.log('wrong values')
+// }
+
+//=====================================================
+
+const name = 'peter';
+const age = 24;
+
+if(name === 'bob' && age === 24) { // if name is equal to bob AND age is equal to 24
+    console.log('hello there user');
+} else {
+    console.log('wrong values')
+}
